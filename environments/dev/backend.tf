7e2b3a642259505a136b47f0b94bd375c30b2898
runtiva <runtiva25@gmail.com>
2023-04-23 16:34:58 +0000

    Update project IDs and buickets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "maximal-evening-384523-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="maximal-evening-384523"
